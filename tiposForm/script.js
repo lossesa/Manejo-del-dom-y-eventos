91aaa97551352d3572cc85a3c2633940b80a3856
sara <sara24>
2025-09-30 20:08:58 -0500

    Se añadio el archivo con los tipos que hay de formulario.

---
+++ tiposForm/script.js
@@ -0,0 +1,13 @@
+// Obtener todos los elementos del formulario con el id.
+const txt = document.getElementById("texto");
+const num = document.getElementById("numero");
+const clave = document.getElementById("clave");
+const sel = document.getElementById("seleccion");
+const txa = document.getElementById("textarea");
+const form = document.getElementById("miForm");
+const rag = document.getElementById("rango");
+const valor = document.getElementById("vlrdato");
+
+
+// Eventos
+
